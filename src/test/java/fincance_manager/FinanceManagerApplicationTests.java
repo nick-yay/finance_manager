e9c40fe0b357bfc29270588922563658ba73263e
nick-yay <nicolasludugerio@gmail.com>
2026-03-10 11:15:05 -0300

    Initialize finance_manager project with Maven setup and Spring Boot application

---
+++ src/test/java/fincance_manager/FinanceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package fincance_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
